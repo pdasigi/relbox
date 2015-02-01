0e3059881f249bd560c6e85ebd75c28509331c63
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2015-02-01 11:52:16 -0500

    "Deeper" model, NCE margin and lielihood losses. Likelihood still needs work.

--- relbox.py
+++ relbox.py
@@ -1,70 +1,72 @@
-import theano
+import theano, numpy
 from theano import tensor as T
 from theano.tensor.shared_randomstreams import RandomStreams
 
 class RelBox(object):
-    def __init__(self, init_box, init_vocab=None, init_rel=None, lr=0.01, dense_vocab_rep=True, dense_rel_rep=False, tensor_activation=T.tanh, train_nce=False):
+    def __init__(self, num_words, num_rels, vocab_embed_size, lr=0.01, tensor_activation=T.tanh, num_noise_samples=1):
+        numpy_rng = numpy.random.RandomState(89677)
+        theano_rng = RandomStreams(12783)
+        rng_box_low = -4 * numpy.sqrt(6. / (vocab_embed_size + vocab_embed_size + num_rels))
+        rng_box_high = 4 * numpy.sqrt(6. / (vocab_embed_size + vocab_embed_size + num_rels))
+        init_box = numpy.asarray(numpy_rng.uniform(low=rng_box_low, high=rng_box_high, size=(vocab_embed_size, vocab_embed_size, num_rels)))
+        rng_proj_low = -4 * numpy.sqrt(6. / (num_words + vocab_embed_size))
+        rng_proj_high = 4 * numpy.sqrt(6. / (num_words + vocab_embed_size))
+        init_dense_vocab = numpy.asarray(numpy_rng.uniform(low=rng_proj_low, high=rng_proj_high, size=(num_words, vocab_embed_size)))
+        init_rev_dense_vocab = numpy.asarray(numpy_rng.uniform(low=rng_proj_low, high=rng_proj_high, size=(vocab_embed_size, num_words)))
         self.B = theano.shared(value=init_box, name='B')
-        word_dim, _, rel_dim = init_box.shape
-        if dense_vocab_rep:
-            self.vocab = theano.shared(value=init_vocab, name='V')
-            word_activation = T.tanh
-        else:
-            self.vocab = T.eye(word_dim)
-            word_activation = T.nnet.softmax
-
-        if dense_rel_rep:
-            self.rel = theano.shared(value=init_rel, name='R')
-            rel_activation = T.tanh
-        else:
-            self.rel = T.eye(rel_dim)
-            rel_activation = T.nnet.softmax
+        self.P = theano.shared(value=init_dense_vocab, name='P')
+        self.P_hat = theano.shared(value=init_rev_dense_vocab, name='P_hat')
+        self.vocab = T.eye(num_words)
+        word_activation = T.nnet.softmax
+        self.rel = T.eye(num_rels)
+        rel_activation = T.nnet.softmax
 
         self.lr = lr
 
         self.x_ind, self.y_ind, self.r_ind = T.iscalars('x_ind', 'y_ind', 'r_ind')
         x = self.vocab[self.x_ind]
+        self.x_rep = T.dot(x, self.P)
         y = self.vocab[self.y_ind]
+        self.y_rep = T.dot(y, self.P)
         r = self.rel[self.r_ind]
         # Assumption: Corresponding dimensions: 0 -> x, 1 -> y, 2 -> r
         # TODO: Where do we apply activations? Do we have to, at all?
         pred_xy = tensor_activation(T.tensordot(r, self.B, axes=(0,2)))
-        self.pred_y = word_activation(T.tensordot(x, pred_xy, axes=(0,0)))
-        self.pred_x = word_activation(T.tensordot(y, pred_xy, axes=(0,1)))
-        pred_yr = tensor_activation(T.tensordot(x, self.B, axes=(0,0)))
-        self.pred_r = rel_activation(T.tensordot(y, pred_yr, axes=(0,0)))
+        pred_y = T.dot(T.tensordot(self.x_rep, pred_xy, axes=(0,0)), self.P_hat)
+        self.prob_y = word_activation(pred_y)
+        pred_x = T.dot(T.tensordot(self.y_rep, pred_xy, axes=(0,1)), self.P_hat)
+        self.prob_x = word_activation(pred_x)
+        pred_yr = tensor_activation(T.tensordot(self.x_rep, self.B, axes=(0,0)))
+        self.prob_r = rel_activation(T.tensordot(self.y_rep, pred_yr, axes=(0,0)))
 
-        if dense_vocab_rep:
-            self.x_loss = self.mse(x, self.pred_x)
-            self.y_loss = self.mse(y, self.pred_y)
-        else:
-            self.x_loss = self.ce(x, self.pred_x)
-            self.y_loss = self.ce(y, self.pred_y)
-        if dense_rel_rep:
-            self.r_loss = self.mse(r, self.pred_r)
-        else:
-            self.r_loss = self.ce(r, self.pred_r)
-        if train_nce:
-            self.score = T.dot(y, T.tensordot(x, T.tensordot(r, self.B, axes=(0,2)), axes=(0,0)).T)
-            srng = RandomStreams(seed=2345)
-            rand_x_ind = srng.random_integers(low=0, high=word_dim-1)
-            rand_y_ind = srng.random_integers(low=0, high=word_dim-1)
-            rand_r_ind = srng.random_integers(low=0, high=rel_dim-1)
+        self.score = T.dot(y, T.dot(T.tensordot(self.x_rep, T.tensordot(r, self.B, axes=(0,2)), axes=(0,0)), self.P_hat).T)
+        # y \times (((x \times P) \times (r \otimes B)) \times P_hat)
+        rand_margin_score = T.constant(0)
+        noise_log_likelihood = T.constant(0)
+        # The noise distribution is one where words and the relation are independent of each other.  The probability of the right tuple and the corrupted tuple are both equal in this distribution.
+        noise_prob = num_noise_samples/float(num_words * num_words * num_rels)
+        for _ in range(num_noise_samples):
+            rand_x_ind = theano_rng.random_integers(low=0, high=num_words-1)
+            rand_y_ind = theano_rng.random_integers(low=0, high=num_words-1)
+            rand_r_ind = theano_rng.random_integers(low=0, high=num_rels-1)
             rand_x = self.vocab[rand_x_ind]
+            rand_x_rep = T.dot(rand_x, self.P)
             rand_y = self.vocab[rand_y_ind]
+            rand_y_rep = T.dot(rand_y, self.P)
             rand_r = self.rel[rand_r_ind]
-            rand_score = T.dot(rand_y, T.tensordot(rand_x, T.tensordot(rand_r, self.B, axes=(0,2)), axes=(0,0)).T)
-            self.nce_loss = T.sum(T.maximum(0, 1 - self.score + rand_score))
-
+            rand_score = T.dot(rand_y, T.dot(T.tensordot(rand_x_rep, T.tensordot(rand_r, self.B, axes=(0,2)), axes=(0,0)), self.P_hat).T)
+            rand_margin_score += rand_score
+            noise_log_likelihood += T.log(noise_prob/(rand_score + noise_prob))
+        self.nce_margin_loss = T.maximum(0, 1 - self.score + rand_margin_score)
+        
+        # NCE negative log likelihood:-1 * {log(score/(score + num_noise_samples*noise_prob)) + \sum_{i=1}^k (log(noise_prob/(rand_score + noise_prob)))}
+        self.nce_prob_loss = -(T.log(self.score/(self.score + noise_prob)) + noise_log_likelihood)
         self.cost_inputs = [self.x_ind, self.y_ind, self.r_ind]
-        self.params = [self.B]
-        if dense_vocab_rep:
-            self.params.append(self.vocab)
+        self.params = [self.B, self.P, self.P_hat]
 
-        if dense_rel_rep:
-            self.params.append(self.rel)
-        #self.params = [self.B, self.vocab, self.rel]
-        #self.params = [self.B, self.vocab]
+        self.x_loss = self.ce(x, self.prob_x)
+        self.y_loss = self.ce(y, self.prob_y)
+        self.r_loss = self.ce(r, self.prob_r)
 
     # TODO: Other (better?) losses
     def mse(self, t, p):
@@ -74,13 +76,13 @@ class RelBox(object):
         return T.mean(T.nnet.binary_crossentropy(p, t))
 
     def get_x_pred(self):
-        return theano.function([self.y_ind, self.r_ind], self.pred_x)
+        return theano.function([self.y_ind, self.r_ind], self.prob_x)
 
     def get_y_pred(self):
-        return theano.function([self.x_ind, self.r_ind], self.pred_y)
+        return theano.function([self.x_ind, self.r_ind], self.prob_y)
 
     def get_r_pred(self):
-        return theano.function([self.x_ind, self.y_ind], self.pred_r)
+        return theano.function([self.x_ind, self.y_ind], self.prob_r)
 
     def get_costs(self):
         return theano.function(self.cost_inputs, [self.x_loss, self.y_loss, self.r_loss])
@@ -106,12 +108,16 @@ class RelBox(object):
             updates_for_r.append((param, param - self.lr * gparam))
         return theano.function(self.cost_inputs, self.r_loss, updates=updates_for_r)
 
-    def train_with_nce(self):
-        gparams = T.grad(self.nce_loss, self.params)
+    def train_with_nce_margin(self):
+        gparams = T.grad(self.nce_margin_loss, self.params)
         updates = []
         for param, gparam in zip(self.params, gparams):
             updates.append((param, param - self.lr * gparam))
-        return theano.function(self.cost_inputs, self.nce_loss, updates=updates)
+        return theano.function(self.cost_inputs, self.nce_margin_loss, updates=updates)
 
-
-
+    def train_with_nce_prob(self):
+        gparams = T.grad(self.nce_prob_loss, self.params)
+        updates = []
+        for param, gparam in zip(self.params, gparams):
+            updates.append((param, param - self.lr * gparam))
+        return theano.function(self.cost_inputs, self.nce_prob_loss, updates=updates)
