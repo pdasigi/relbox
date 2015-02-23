56c5882b26a3dad7db5e32aec34827b016eb6da2
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2015-02-23 11:55:34 -0500

    Can use pretrained vectors for initializing P

--- predict.py
+++ predict.py
@@ -1,40 +1,88 @@
-import numpy, theano, sys
+import numpy, theano, sys, re
 from relbox import RelBox
-import cPickle
+import pickle
 
-vocab = [x.strip() for x in open("vocab.txt")]
-rels = [x.strip() for x in open("rels.txt")]
+dirname = "learned_param5M+187k_500k_glove/"
+#dirname = ""
+dignorm = False
+vocab = []
+word_dim = 50
+for wrdline in open(dirname+"vocab.txt"):
+    lnparts = wrdline.strip().split()
+    if lnparts[-1] != 'OOV':
+        vocab.append(lnparts[0])
+vocab.append('UNK')
+rels = [x.strip() for x in open(dirname+"rels.txt")]
 #learned_vocab = cPickle.load(open("learned_vocab.pkl", "rb"))
-learned_box = cPickle.load(open("box.pkl", "rb"))
+learned_box, learned_proj, learned_rev_proj = pickle.load(open(dirname+"box_param_glove_5.pkl", "rb"))
 rel_rep = numpy.eye(len(rels), dtype=theano.config.floatX)
 vocab_rep = numpy.eye(len(vocab)+1, dtype=theano.config.floatX)
 
 #relbox = RelBox(learned_box, learned_vocab, rel_rep)
-relbox = RelBox(learned_box, vocab_rep, rel_rep)
+relbox = RelBox(len(vocab), len(rels), word_dim)
+relbox.B.set_value(learned_box)
+relbox.P.set_value(learned_proj)
+relbox.P_hat.set_value(learned_rev_proj)
 get_r_pred = relbox.get_r_pred()
 get_x_pred = relbox.get_x_pred()
 get_y_pred = relbox.get_y_pred()
-testfile = open("tuples_test.txt")
+get_score = relbox.get_score()
+testfile = open(sys.argv[1])
 
 for line in testfile:
     #line = sys.stdin.readline()
     lnstrp = line.strip()
-    if lnstrp == "":
-        break
-    t_x, t_r, t_y, _ = lnstrp.split()
-    try:
-        x_ind = vocab.index(t_x.lower())
-        y_ind = vocab.index(t_y.lower())
-        r_ind = rels.index(t_r.lower())
-    except ValueError, e:
-        #print >>sys.stderr, "One of %s %s and %s is not in index"%(x, y, r), e
+    #if lnstrp == "":
+    #    break
+    parts = lnstrp.split()
+    tuples = []
+    if len(parts) < 3:
+        print "0.0"
         continue
-    r_pred = get_r_pred(x_ind, y_ind)
-    x_pred = get_x_pred(y_ind, r_ind)
-    y_pred = get_y_pred(x_ind, r_ind)
-    rel_probs = sorted(zip(r_pred[0], rels), reverse=True)
-    print "r\t%s\t%s\t%s\t%s\t%f"%(t_x, t_y, t_r, rel_probs[0][1], rel_probs[0][0])
-    x_probs = sorted(zip(x_pred, vocab+['unk']), reverse=True)
-    print "x\t%s\t%s\t%s\t%f\t%s"%(t_r, t_y, t_x, vocab.index(t_x), "\t".join(["%f\t%s"%(p, x) for (p, x) in x_probs[:5]]))
-    y_probs = sorted(zip(y_pred, vocab+['unk']), reverse=True)
-    print "y\t%s\t%s\t%s\t%f\t%s"%(t_x, t_r, t_y, vocab.index(t_y), "\t".join(["%f\t%s"%(p, y) for (p, y) in y_probs[:5]]))
+    if len(parts) > 4 and len(parts)%3 == 0:
+        if dignorm:
+            for i in range(0, len(parts), 3):
+                tuples.append((re.sub("[0-9]", "D", parts[i]), parts[i+1], re.sub("[0-9]", "D", parts[i+2])))
+        else:
+            for i in range(0, len(parts), 3):
+                tuples.append((parts[i], parts[i+1], parts[i+2]))
+    else:
+        if dignorm:
+            tuples.append((re.sub("[0-9]", "D", parts[0]), parts[1], re.sub("[0-9]", "D", parts[2])))
+        else:
+            tuples.append((parts[0], parts[1], parts[2]))
+    scores = []
+    for t_x, t_r, t_y in tuples:
+        try:
+            x_ind = vocab.index(t_x.lower())
+        except:
+            x_ind = len(vocab) - 1
+        try:
+            y_ind = vocab.index(t_y.lower())
+        except:
+            y_ind = len(vocab) - 1
+        try:
+            r_ind = rels.index(t_r.lower())
+        except ValueError, e:
+            #print >>sys.stderr, "One of %s %s and %s is not in index"%(x, y, r), e
+            continue
+        scores.append(get_score(x_ind, y_ind, r_ind))
+        """r_pred = get_r_pred(x_ind, y_ind)
+        x_pred = get_x_pred(y_ind, r_ind)
+        y_pred = get_y_pred(x_ind, r_ind)
+        rel_probs = sorted(zip(r_pred[0], rels), reverse=True)
+        print "r\t%s\t%s\t%s\t%s\t%f"%(t_x, t_y, t_r, rel_probs[0][1], rel_probs[0][0])
+        x_probs = sorted(zip(x_pred[0], vocab), reverse=True)
+        corr_x_ind = len(vocab) - 1
+        for ind, (_, w) in enumerate(x_probs):
+            if t_x == w:
+                corr_x_ind = ind + 1
+        print "x\t%s\t%s\t%s\t%d\t%s"%(t_r, t_y, t_x, corr_x_ind, "\t".join(["%f\t%s"%(p, x) for (p, x) in x_probs[:5]]))
+        y_probs = sorted(zip(y_pred[0], vocab), reverse=True)
+        corr_y_ind = len(vocab) - 1
+        for ind, (_, w) in enumerate(y_probs):
+            if t_y == w:
+                corr_y_ind = ind + 1
+        print "y\t%s\t%s\t%s\t%d\t%s"%(t_x, t_r, t_y, corr_y_ind, "\t".join(["%f\t%s"%(p, y) for (p, y) in y_probs[:5]]))"""
+    #print " ".join(["%.5f %s %s %s"%(score, t_x, t_r, t_y) for score, (t_x, t_r, t_y) in zip(scores, tuples)])
+    print " ".join(["%.5f"%score for score in scores])
--- run_relbox.py
+++ run_relbox.py
@@ -1,35 +1,23 @@
-import sys, numpy, theano, random
+import sys, random, gzip
 from relbox import RelBox
 import cPickle
-
-numpy_rng = numpy.random.RandomState(89677)
-
-def get_vocab_rep(vocab, words_file, embed_file, dim=50):
-    init_map = {word: numpy.asarray([float(y) for y in embed.strip().split()]) for word, embed in zip(open(words_file), open(embed_file))}
-    vocab_rep = []
-    rng_low = -4 * numpy.sqrt(6. / dim)
-    rng_high = 4 * numpy.sqrt(6. / dim)
-    for word in vocab:
-        if word in init_map:
-            vocab_rep.append(init_map[word])
-        else:
-            vocab_rep.append(numpy.asarray(numpy_rng.uniform(low=rng_low, high=rng_high, size=dim)))
-    return numpy.asarray(vocab_rep, dtype=theano.config.floatX)
+import operator
+import numpy
 
 if __name__ == "__main__":
-    dense_vocab_rep = False
-    dense_rel_rep = False
     word_dim = 50
-    #rel_dim = 2
+    oov_cond = "prop" # prop or freq
+    oov_prop = 0.1 # oov_prop of the words with low frequencies will be considered oov
+    oov_freq_limit = 2
     train_prop = 0.8
     num_iter = 100
-    data_file = "data/tuples_train_dev.txt"
-    if dense_vocab_rep:
-        words_file = "words.lst"
-        embed_file = "embeddings.txt"
+    init_P = "rand" # rand or embed
+    numpy_rng = numpy.random.RandomState(89677)
+    data_file = sys.argv[1]
     infile = open(data_file)
     all_tuples = []
-    vocab = set([])
+    vocab_freq_map = {}
+    oov_words = [] 
     rels = set([])
     train_tuple_inds = []
     valid_tuple_inds = []
@@ -37,73 +25,115 @@ if __name__ == "__main__":
         sub, rel, obj, cnt_st = line.strip().split('\t')
         cnt = int(cnt_st)
         all_tuples.extend([(sub, rel, obj)]*cnt)
-        vocab.add(sub)
-        vocab.add(obj)
+        #all_tuples.extend([(sub, rel, obj)])
+        if sub in vocab_freq_map:
+            vocab_freq_map[sub] += cnt
+        else:
+            vocab_freq_map[sub] = cnt
+        if obj in vocab_freq_map:
+            vocab_freq_map[obj] += cnt
+        else:
+            vocab_freq_map[obj] = cnt
         rels.add(rel)
-    vocab = list(vocab)
-    rels = list(rels)
-    vocab_size = len(vocab)
-    rel_size = len(rels)
-    #init_box_rep = numpy.random.random((word_dim, word_dim, rel_dim))
-    # Key idea: Since rel representation is one-hot, rel_dim = rel_size
-    #init_box_rep = numpy.random.random((word_dim, word_dim, rel_size))
-    #init_vocab_rep = numpy.random.random((vocab_size, dim))
-    if dense_vocab_rep:
-        init_vocab_rep = get_vocab_rep(vocab, words_file, embed_file, word_dim)
-        box_rng_low = -4 * numpy.sqrt(6. / (word_dim + word_dim + rel_size))
-        box_rng_high = 4 * numpy.sqrt(6. / (word_dim + word_dim + rel_size))
-        init_box_rep = numpy.asarray(numpy_rng.uniform(low=box_rng_low, high=box_rng_high, size=(word_dim, word_dim, rel_size)), dtype=theano.config.floatX)
+    vocab = {} # vocab is a map : word -> index
+    sorted_vocab = sorted(vocab_freq_map.items(), key=operator.itemgetter(1))
+    init_vocab_size = len(vocab_freq_map)
+    if oov_cond == "prop":
+        oov_num = int(len(sorted_vocab) * oov_prop)
+        vocab_index = 0
+        for ind, (word, freq) in enumerate(sorted_vocab):
+            if ind > oov_num:
+                #vocab.append(word)
+                vocab[word] = vocab_index
+                vocab_index += 1
+            else:
+                oov_words.append(word)
+        print >>sys.stderr, "Considering the bottom %f as OOV"%(oov_prop)
     else:
-        # vocab_size '+ 1' for unk
-        sparse_word_dim = vocab_size + 1
-        init_vocab_rep = numpy.eye(sparse_word_dim, dtype=theano.config.floatX)
-        box_rng_low = -4 * numpy.sqrt(6. / (sparse_word_dim + sparse_word_dim + rel_size))
-        box_rng_high = 4 * numpy.sqrt(6. / (sparse_word_dim + sparse_word_dim + rel_size))
-        init_box_rep = numpy.asarray(numpy_rng.uniform(low=box_rng_low, high=box_rng_high, size=(sparse_word_dim, sparse_word_dim, rel_size)), dtype=theano.config.floatX)
-    print >>sys.stderr, "Initialized Relation Box"
-    print >>sys.stderr, "Created initial vocab representation"
-    #init_rel_rep = numpy.random.random((rel_size, rel_dim))
-    init_rel_rep = numpy.eye(rel_size, dtype=theano.config.floatX)
-    print >>sys.stderr, "Created initial relation representation"
+        vocab_index = 0
+        for word, freq in sorted_vocab:
+            if freq >= oov_freq_limit:
+                #vocab.append(word)
+                vocab[word] = vocab_index
+                vocab_index += 1
+            else:
+                oov_words.append(word)
+        print >>sys.stderr, "Considering words with frequency less than %d as OOV"%(freq)
+    vocab_size = len(vocab)
+    #vocab.append('UNK') # Last word in UNK
+    vocab['UNK'] = vocab_size # Last word in UNK
+    vocab_size += 1
+    print >>sys.stderr, "Vocab size changed from %d to %d"%(init_vocab_size, vocab_size)
+    rels = list(rels)
+    words_file = open("vocab.txt", "w")
+    for word, freq in sorted_vocab:
+        if word in oov_words:
+            print >>words_file, word, freq, "OOV"
+        else:
+            print >>words_file, word, freq
+    words_file.close()
+    rels_file = open("rels.txt", "w")
+    for rel in rels:
+        print >>rels_file, rel
+    rels_file.close()
+    rel_size = len(rels)
     random.shuffle(all_tuples)
     data_size = len(all_tuples)
     train_size = int(data_size * train_prop)
     valid_size = data_size - train_size
     for i, (sub, rel, obj) in enumerate(all_tuples):
-        tup_rep = (vocab.index(sub), rels.index(rel), vocab.index(obj))
+        if sub in vocab:
+            #sub_ind = vocab.index(sub)
+            sub_ind = vocab[sub]
+        else:
+            sub_ind = vocab['UNK']
+        if obj in vocab:
+            #obj_ind = vocab.index(obj)
+            obj_ind = vocab[obj]
+        else:
+            #obj_ind = len(vocab) - 1
+            obj_ind = vocab['UNK']
+        tup_rep = (sub_ind, rels.index(rel), obj_ind)
         if i < train_size:
             train_tuple_inds.append(tup_rep)
         else:
             valid_tuple_inds.append(tup_rep)
-    #relbox = RelBox(init_box_rep, init_vocab_rep, init_rel_rep, dense_vocab_rep=dense_vocab_rep, dense_rel_rep=dense_rel_rep)
-    #x_update = relbox.update_for_x()
-    #y_update = relbox.update_for_y()
-    #r_update = relbox.update_for_r()
-    relbox = RelBox(init_box_rep, dense_vocab_rep=False, dense_rel_rep=False, train_nce=True)
-    nce_train_fun = relbox.train_with_nce()
+    if init_P == "embed":
+        print >>sys.stderr, "Initializing projection with embedding at %s"%(sys.argv[2])
+        randrep = numpy.asarray(numpy_rng.uniform(low = -4 * numpy.sqrt(6. / word_dim), high = 4 * numpy.sqrt(6. / word_dim), size=word_dim))
+        vocab_rep = [randrep] * vocab_size
+        embed_file = gzip.open(sys.argv[2])
+        in_vocab = set()
+        for line in embed_file:
+            lineparts = line.strip().split()
+            wrd = lineparts[0]
+            if wrd in vocab:
+                vocab_rep[vocab[wrd]] = numpy.asarray([float(x) for x in lineparts[1:]])
+                in_vocab.add(wrd)
+        print >>sys.stderr, "Embedding has %f coverage"%(float(len(in_vocab))/vocab_size)
+        relbox = RelBox(vocab_size, rel_size, word_dim, num_noise_samples=1, init_dense_vocab=numpy.asarray(vocab_rep))
+        pkl_ext = sys.argv[2].split('/')[-1].split('.')[0]
+    else:
+        relbox = RelBox(vocab_size, rel_size, word_dim, num_noise_samples=1)
+        pkl_ext = "rand"
+        print >>sys.stderr, "Initializing projection randomly"
+    nce_train_fun = relbox.train_with_nce_margin()
     get_costs = relbox.get_costs()
     for i in range(num_iter):
-        """x_train_costs = []
-        y_train_costs = []
-        r_train_costs = []
-        for s_ind, r_ind, o_ind in train_tuple_inds:
-            x_loss = x_update(s_ind, o_ind, r_ind)
-            x_train_costs.append(x_loss)
-        for s_ind, r_ind, o_ind in train_tuple_inds:
-            y_loss = y_update(s_ind, o_ind, r_ind)
-            y_train_costs.append(y_loss)
-        for s_ind, r_ind, o_ind in train_tuple_inds:
-            r_loss = r_update(s_ind, o_ind, r_ind)
-            r_train_costs.append(r_loss)
-        avg_train_cost = (sum(x_train_costs)/train_size, sum(y_train_costs)/train_size, sum(r_train_costs)/train_size)"""
         nce_losses = []
         for s_ind, r_ind, o_ind in train_tuple_inds:
             nce_loss = nce_train_fun(s_ind, o_ind, r_ind)
             nce_losses.append(nce_loss)
         avg_train_cost = sum(nce_losses)/train_size
-        #avg_train_cost = ((sum([x[0] for x in train_costs])+0.0)/train_size, (sum([x[1] for x in train_costs])+0.0)/train_size, (sum([x[2] for x in train_costs])+0.0)/train_size)
-        #print >>sys.stderr, "Finished epoch %d, train costs are %f, %f, %f"%(i+1, avg_train_cost[0], avg_train_cost[1], avg_train_cost[2])
         print >>sys.stderr, "Finished epoch %d, train cost is %f"%(i+1, avg_train_cost)
+        if avg_train_cost == 0:
+            break
+        if (i+1)%5 == 0:
+            learned_param = (relbox.B.get_value(), relbox.P.get_value(), relbox.P_hat.get_value())
+            box_file = open("box_param_%s_%d.pkl"%(pkl_ext, i+1), "wb")
+            cPickle.dump(learned_param, box_file)
+            print >>sys.stderr, "Dumped relbox"
+            box_file.close()
         if (i+1)%10 == 0:
             valid_costs = []
             for s_ind, r_ind, o_ind in valid_tuple_inds:
@@ -112,21 +142,7 @@ if __name__ == "__main__":
 
             avg_valid_cost = ((sum([x[0] for x in valid_costs])+0.0)/valid_size, (sum([x[1] for x in valid_costs])+0.0)/valid_size, (sum([x[2] for x in valid_costs])+0.0)/valid_size)
             print >>sys.stderr, "\tvalid costs are %f, %f, %f"%(avg_valid_cost[0], avg_valid_cost[1], avg_valid_cost[2])
-    words_file = open("vocab.txt", "w")
-    for word in vocab:
-        print >>words_file, word
-    words_file.close()
-    rels_file = open("rels.txt", "w")
-    for rel in rels:
-        print >>rels_file, rel
-    rels_file.close()
-    learned_box = relbox.B.get_value()
-    learned_vocab = relbox.vocab.get_value()
-    learned_rel = relbox.rel.get_value()
-    box_file = open("nce_box.pkl", "wb")
-    cPickle.dump(learned_box, box_file)
-    #vocab_file = open("learned_vocab.pkl", "wb")
-    #cPickle.dump(learned_vocab, vocab_file)
-    #rel_file = open("learned_rel.pkl", "wb")
-    #cPickle.dump(learned_rel, rel_file)
+    learned_param = (relbox.B.get_value(), relbox.P.get_value(), relbox.P_hat.get_value())
+    box_file = open("box_param_%s.pkl"%(pkl_ext), "wb")
+    cPickle.dump(learned_param, box_file)
     print >>sys.stderr, "Done!"
